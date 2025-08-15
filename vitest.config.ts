0f35f181bff07b2ee493fbcc6b3bd72e61d0cd60
Lollipope <qinye5@qq.com>
2025-08-15 17:05:25 +0800

    wip:添加相关配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,9 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
-  viteConfig,
+  {},
   defineConfig({
+    resolve: viteConfig.resolve,
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
