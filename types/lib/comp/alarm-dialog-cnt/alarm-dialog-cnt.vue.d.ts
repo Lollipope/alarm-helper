70217c91c26bc01cae20caf98df5c9251317facf
qinye5 <qinye5@qq.com>
2025-08-22 22:54:53 +0800

    docs: rebuild *.d.ts

--- types/lib/comp/alarm-dialog-cnt/alarm-dialog-cnt.vue.d.ts
+++ types/lib/comp/alarm-dialog-cnt/alarm-dialog-cnt.vue.d.ts
@@ -74,8 +74,6 @@ declare const _default: import('vue').DefineComponent<{}, {
                     from<T>(iterable: Iterable<T> | ArrayLike<T>): T[];
                     from<T, U>(iterable: Iterable<T> | ArrayLike<T>, mapfn: (v: T, k: number) => U, thisArg?: any): U[];
                     of<T>(...items: T[]): T[];
-                    fromAsync<T>(iterableOrArrayLike: AsyncIterable<T> | Iterable<T | PromiseLike<T>> | ArrayLike<T | PromiseLike<T>>): Promise<T[]>;
-                    fromAsync<T, U>(iterableOrArrayLike: AsyncIterable<T> | Iterable<T> | ArrayLike<T>, mapFn: (value: Awaited<T>, index: number) => U, thisArg?: any): Promise<Awaited<U>[]>;
                     readonly [Symbol.species]: ArrayConstructor;
                 };
                 default: () => never[];
@@ -138,8 +136,6 @@ declare const _default: import('vue').DefineComponent<{}, {
                     from<T>(iterable: Iterable<T> | ArrayLike<T>): T[];
                     from<T, U>(iterable: Iterable<T> | ArrayLike<T>, mapfn: (v: T, k: number) => U, thisArg?: any): U[];
                     of<T>(...items: T[]): T[];
-                    fromAsync<T>(iterableOrArrayLike: AsyncIterable<T> | Iterable<T | PromiseLike<T>> | ArrayLike<T | PromiseLike<T>>): Promise<T[]>;
-                    fromAsync<T, U>(iterableOrArrayLike: AsyncIterable<T> | Iterable<T> | ArrayLike<T>, mapFn: (value: Awaited<T>, index: number) => U, thisArg?: any): Promise<Awaited<U>[]>;
                     readonly [Symbol.species]: ArrayConstructor;
                 };
                 default: () => never[];
