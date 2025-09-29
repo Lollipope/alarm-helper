35660952da0cb4adebaf5c6488d9d25a626be6f4
qinye5 <qinye5@qq.com>
2025-09-29 22:01:20 +0800

    wip: define new type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,25 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  // rules: {
-  //   // 自定义规则示例
-  //   'type-enum': [
-  //     2,
-  //     'always',
-  //     ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
-  //   ],
-  //   'subject-case': [0], // 允许任意大小写
-  // },
+  rules: {
+    // 自定义规则示例
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'wip',
+      ],
+    ],
+    'subject-case': [0], // 允许任意大小写
+  },
 }
