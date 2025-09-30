cec80d048cc4db09c3dcf0d74f08cca3e8bf8783
Lollipope <qinye5@qq.com>
2025-09-30 11:16:24 +0800

    refactor: ci/cd

---
+++ .changelog.config.js
@@ -0,0 +1,9 @@
+export default {
+  writerOpts: {
+    transform: (commit) => {
+      const skipTypes = ['chore', 'test', 'docs', 'wip']
+      if (!commit.type || skipTypes.includes(commit.type)) return false
+      return commit
+    },
+  },
+}
